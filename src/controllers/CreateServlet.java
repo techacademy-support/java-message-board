892f0d78cf7de4c1bff7402ae8ff5cab635b16f1
techacademy-support <support@techacademy.jp>
2019-02-12 18:17:39 +0900

    Add CreateServlet.java

---
+++ src/controllers/CreateServlet.java
@@ -0,0 +1,63 @@
+package controllers;
+
+import java.io.IOException;
+import java.sql.Timestamp;
+
+import javax.persistence.EntityManager;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import models.Message;
+import utils.DBUtil;
+
+/**
+ * Servlet implementation class CreateServlet
+ */
+@WebServlet("/create")
+public class CreateServlet extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public CreateServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+    /**
+     * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+     */
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String _token = (String)request.getParameter("_token");
+        if(_token != null && _token.equals(request.getSession().getId())) {
+            EntityManager em = DBUtil.createEntityManager();
+
+            // インスタンスを生成して各フィールドに代入
+            Message m = new Message();
+
+            String title = request.getParameter("title");
+            m.setTitle(title);
+
+            String content = request.getParameter("content");
+            m.setContent(content);
+
+            Timestamp currentTime = new Timestamp(System.currentTimeMillis());     // 現在日時
+            m.setCreated_at(currentTime);
+            m.setUpdated_at(currentTime);
+
+            // データベースに保存
+            em.getTransaction().begin();
+            em.persist(m);
+            em.getTransaction().commit();
+            em.close();
+
+            // indexのページにリダイレクト
+            response.sendRedirect(request.getContextPath() + "/index");
+        }
+    }
+
+}
